627aea5149c831a0592f12e7f42618379e4adad3
t0bst4r <t0bst4r@outlook.com>
2024-10-28 10:12:42 +0100

    fix(basic-information): consider maxLength of device properties
    fixes #6

--- packages/backend/src/matter/behaviors/basic-information-server.ts
+++ packages/backend/src/matter/behaviors/basic-information-server.ts
@@ -4,6 +4,7 @@ import {
   HomeAssistantEntityState,
 } from "@home-assistant-matter-hub/common";
 import { Behavior } from "@project-chip/matter.js/behavior";
+import crypto from "node:crypto";
 
 export class BasicInformationServer extends Base {}
 
@@ -14,13 +15,32 @@ export namespace BasicInformationServer {
   ): Behavior.Options<typeof BasicInformationServer> {
     return {
       vendorId: basicInformation.vendorId,
-      vendorName: basicInformation.vendorName,
-      productName: basicInformation.productName,
-      productLabel: basicInformation.productLabel,
+      vendorName: maxLengthOrHash(basicInformation.vendorName, 32),
+      productName: maxLengthOrHash(basicInformation.productName, 32),
+      productLabel: maxLengthOrHash(basicInformation.productLabel, 64),
       hardwareVersion: basicInformation.hardwareVersion,
       softwareVersion: basicInformation.softwareVersion,
-      nodeLabel: state.attributes.friendly_name ?? "Unknown Entity",
+      nodeLabel: maxLengthOrHash(
+        state.attributes.friendly_name ?? "Unknown Entity",
+        32,
+      ),
       reachable: true,
     };
+  }
+}
+
+function maxLengthOrHash(value: string, maxLength: number): string {
+  if (maxLength < 16) {
+    throw new Error("MaxLength cannot be shorter than 16");
+  }
+  if (value.length < maxLength) {
+    return value;
+  } else {
+    const hash = crypto
+      .createHash("md5")
+      .update(value)
+      .digest("hex")
+      .substring(0, 8);
+    return value.substring(0, maxLength - 8) + hash;
   }
 }
