963d3b25fd6f0f42e411e30d71491d6ff876a034
Tobias Glatthar <t0bst4r@outlook.com>
2024-11-27 16:28:07 +0100

    fix: add more details when home assistant connection fails

--- packages/backend/src/home-assistant/home-assistant-client.ts
+++ packages/backend/src/home-assistant/home-assistant-client.ts
@@ -2,6 +2,8 @@ import {
   Connection,
   createConnection,
   createLongLivedTokenAuth,
+  ERR_CANNOT_CONNECT,
+  ERR_INVALID_AUTH,
 } from "home-assistant-js-websocket";
 import { Environment, Environmental } from "@matter/main";
 import { register } from "../environment/register.js";
@@ -29,10 +31,26 @@ export class HomeAssistantClient implements Environmental.Service {
         this.props.url.replace(/\/$/, ""),
         this.props.accessToken,
       ),
+    }).catch((reason) => {
+      throw this.parseError(reason);
     });
   }
 
   async [Symbol.asyncDispose]() {
     this.connection?.close();
+  }
+
+  private parseError(reason: unknown): Error {
+    if (reason === ERR_CANNOT_CONNECT) {
+      return new Error(
+        `Unable to connect to home assistant with url: ${this.props.url}`,
+      );
+    } else if (reason === ERR_INVALID_AUTH) {
+      return new Error(
+        "Authentication failed while connecting to home assistant",
+      );
+    } else {
+      return new Error(`Unable to connect to home assistant: ${reason}`);
+    }
   }
 }
