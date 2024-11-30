42cdb965e7295ae90c4076d6eb1a726e48dbda88
Tobias Glatthar <t0bst4r@outlook.com>
2024-11-30 20:22:40 +0100

    chore(ci): remove nx recording for prettier in pull requests

--- packages/frontend/src/components/fabric/FabricIcon.tsx
+++ packages/frontend/src/components/fabric/FabricIcon.tsx
@@ -5,34 +5,39 @@ import { faGoogle } from "@fortawesome/free-brands-svg-icons/faGoogle";
 import { Tooltip } from "@mui/material";
 import QuestionMark from "@mui/icons-material/QuestionMark";
 import { BridgeFabric } from "@home-assistant-matter-hub/common";
-import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+import { IconLookup } from "@fortawesome/fontawesome-svg-core";
+import { useMemo } from "react";
 
 export interface FabricIconProps {
   fabric: BridgeFabric;
 }
 
-const iconsPerKeyword: Record<string, IconDefinition> = {
+const iconsPerKeyword: Record<string, IconLookup> = {
   Alexa: faAmazon,
   Apple: faApple,
   Google: faGoogle,
 };
 
-const vendorIdFallback: Record<number, IconDefinition | undefined> = {
+const iconPerVendorId: Record<number, IconLookup | undefined> = {
   0x1217: faAmazon,
   0x1349: faApple,
   0x1384: faApple,
   0x6006: faGoogle,
+  // 0x10e1: "Samsung SmartThings",
 };
 
-export const FabricIcon = ({ fabric }: FabricIconProps) => {
-  let icon = Object.entries(iconsPerKeyword).find(([keyword]) =>
+function getIcon(fabric: BridgeFabric): IconLookup | undefined {
+  const icon = iconPerVendorId[fabric.rootVendorId];
+  if (icon) {
+    return icon;
+  }
+  return Object.entries(iconsPerKeyword).find(([keyword]) =>
     fabric.label.toUpperCase().includes(keyword.toUpperCase()),
   )?.[1];
+}
 
-  if (!icon) {
-    icon = vendorIdFallback[fabric.rootVendorId];
-  }
-
+export const FabricIcon = ({ fabric }: FabricIconProps) => {
+  const icon = useMemo(() => getIcon(fabric), [fabric]);
   return (
     <Tooltip title={`${fabric.label} (${fabric.rootVendorId})`} arrow>
       <span>{icon ? <FontAwesomeIcon icon={icon} /> : <QuestionMark />}</span>
