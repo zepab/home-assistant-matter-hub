57465a635f9fffe10156b36ff2d7aacd5a2dcfa3
Tobias Glatthar <t0bst4r@outlook.com>
2025-01-06 14:34:12 +0100

    fix(input_button): turn the matter device 'on' for a short time
    fixes #388

--- packages/backend/src/matter/behaviors/auto-off-server.ts
+++ packages/backend/src/matter/behaviors/auto-off-server.ts
@@ -1,5 +1,6 @@
 import { OnOffServer as Base } from "@matter/main/behaviors";
 import { HomeAssistantEntityBehavior } from "../custom-behaviors/home-assistant-entity-behavior.js";
+import { HomeAssistantEntityInformation } from "@home-assistant-matter-hub/common";
 
 export interface AutoOffConfig {
   turnOn?: {
@@ -10,21 +11,28 @@ export interface AutoOffConfig {
 
 export class AutoOffServer extends Base {
   declare state: AutoOffServer.State;
+  declare internal: AutoOffServer.Internal;
 
   override async initialize() {
     super.initialize();
-    await this.agent.load(HomeAssistantEntityBehavior);
+    const homeAssistant = await this.agent.load(HomeAssistantEntityBehavior);
+    this.update(homeAssistant.entity);
+    this.reactTo(homeAssistant.onChange, this.update);
+  }
+
+  private update(entity: HomeAssistantEntityInformation) {
+    this.internal.turnOffTimeout = this.state.config?.turnOn?.timeout ?? 1000;
+    const lastPressed = new Date(entity.state.state).getTime();
+    this.state.onOff =
+      !isNaN(lastPressed) &&
+      Date.now() - lastPressed < this.internal.turnOffTimeout;
   }
 
   override async on() {
     const homeAssistant = this.agent.get(HomeAssistantEntityBehavior);
     const action = this.state.config?.turnOn?.action ?? "homeassistant.turn_on";
     await homeAssistant.callAction(action);
-
-    setTimeout(
-      this.callback(this.off),
-      this.state.config?.turnOn?.timeout ?? 1000,
-    );
+    setTimeout(this.callback(this.off), this.internal.turnOffTimeout);
   }
 
   override async off() {
@@ -35,5 +43,9 @@ export class AutoOffServer extends Base {
 export namespace AutoOffServer {
   export class State extends Base.State {
     config?: AutoOffConfig;
+  }
+
+  export class Internal extends Base.Internal {
+    turnOffTimeout!: number;
   }
 }
